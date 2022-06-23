f0ffbb61fa80ab5e6a1007c9e0c2a99f8cb9fa4b
olayinka olamijulo <olayinkaolamijuloo@gmail.com>
2022-06-23 11:54:09 +0100

    Print basic informations

---
+++ beginner.js
@@ -0,0 +1,14 @@
+// print information
+let student = {
+    name : "Olamijulo Olayinka",
+    height : "1.8 meters",
+    country : "Nigeria",
+}
+
+// document.write(student.name);
+// document.write(student.height);
+// document.write(student.country);
+
+console.log("Name:    " + student.name);
+console.log("Height:  " + student.height);
+console.log("Country: " + student.country);
